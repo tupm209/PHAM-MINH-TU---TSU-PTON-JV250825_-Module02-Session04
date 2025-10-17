72228803f2f069a458e00b2c8d671747d403b019
tupm209 <tu.pm209@gmail.com>
2025-10-18 02:05:47 +0900

    Add files via upload

---
+++ hw01.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA ss4_hw01;
+USE ss4_hw01;
+
+CREATE TABLE Students (
+	`studentId` INT PRIMARY KEY,
+    `name` VARCHAR (50) NOT NULL,
+    `age` INT NOT NULL,
+    `major` VARCHAR (50)
+);
+
+
+INSERT INTO Students VALUES (1, "Alice", 20, "Computer Science");
+INSERT INTO Students VALUES (2, "Bob", 22, "Mathematics");
+INSERT INTO Students VALUES (3, "Charlie", 21, "Physics");
